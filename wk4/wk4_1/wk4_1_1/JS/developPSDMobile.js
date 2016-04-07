606301078a69ab0fd573633e53bcd263870bdd47
Alice0424 <13011685296@163.com>
2016-04-07 22:13:15 +0800

    开发极客学院首页或者自己寻找PSDv1.0

---
+++ wk4/wk4_1/wk4_1_1/JS/developPSDMobile.js
@@ -0,0 +1,38 @@
+/**
+* Created by xiaojuan on 2015/12/17.
+*/
+function showPicture1()
+{
+    document.getElementById("bgImg").style.background = "url(images/fur5.jpg)";
+    //document.getElementById("bgImg").style.backgroundSize="contain";
+    document.getElementById("bgImg").style.backgroundRepeat="no-repeat";
+    document.getElementById("bgImg").style.backgroundPosition="center center";
+    document.getElementById("button1").style.background="#1dffdc";
+    document.getElementById("button2").style.background="#FFFFFF";
+    document.getElementById("button3").style.background="#ffffff";
+
+    //background-size: contain;
+    //background-position: center center;
+}
+
+function showPicture2()
+{
+    document.getElementById("bgImg").style.background = "url(images/fur6.jpg)";
+    //document.getElementById("bgImg").style.backgroundSize="contain";
+    document.getElementById("bgImg").style.backgroundRepeat="no-repeat";
+    document.getElementById("bgImg").style.backgroundPosition="center center";
+    document.getElementById("button1").style.background="#ffffff";
+    document.getElementById("button2").style.background="#1dffdc";
+    document.getElementById("button3").style.background="#ffffff";
+}
+
+function showPicture3()
+{
+    document.getElementById("bgImg").style.background = "url(images/fur7.jpg)";
+    //document.getElementById("bgImg").style.backgroundSize="contain";
+    document.getElementById("bgImg").style.backgroundRepeat="no-repeat";
+    document.getElementById("bgImg").style.backgroundPosition="center center";
+    document.getElementById("button1").style.background="#ffffff";
+    document.getElementById("button2").style.background="#ffffff";
+    document.getElementById("button3").style.background="#1dffdc";
+}
